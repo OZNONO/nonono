2f7990e94ef8b78388ee87a02895e87e3ce7804f
OZNONO <62945266+OZNONO@users.noreply.github.com>
2021-06-28 17:02:18 +0900

    20210628 - 1
    expired domain

--- maincontents.js
+++ maincontents.js
@@ -1,12 +1,12 @@
 jQuery(".nav-link").hover( function () { jQuery(this).addClass("active"); }, function () { jQuery(this).removeClass("active"); } );
 function loadsogae() {
  $("#bonmoon").empty();
- bonmoon.innerHTML = "<p>軍人 2019.05.27~2020.12.25</p>\
+ bonmoon.innerHTML = "<p>갹</p>\
  <p>Contact : open.kakao.com/me/OZNONO</p>\
  <p>twitter : @ji112</p>\
  <p>개인 웹사이트 입니다. 모 동네의 블러드캣에서 영감을 받았대요.</p>\
- <p><button class=\"btn btn-default border\" onclick=\"location.href='http://oznono.com/sdvx';\">SDVX</button></p>\
- <p><button class=\"btn btn-default border\" onclick=\"location.href='http://oznono.com/popn';\">pop'n</button></p>\
+ <p><button class=\"btn btn-default border\" onclick=\"location.href='./sdvx/index.html';\">SDVX</button></p>\
+ <p><button class=\"btn btn-default border\" onclick=\"location.href='./popn/index.html';\">pop'n</button></p>\
 ";
 };
 
@@ -14,14 +14,14 @@ function loadsogae() {
 function loadoz() {
    $("#bonmoon").empty();
    bonmoon.innerHTML = "\<p>since 2018.08.05~</p>\
-<p>팀장을 맡고 있습니다. 군인이 늘어나고 있습니다.</p>\
+<p>팀장을 맡고 있습니다. 전역자가 늘어나고 있습니다..</p>\
 <p>문의사항은 연락 주세요.</p>\
 <p>2020.05 멤버수 증원 (30->35人)</p>\
-<p>명단을 적고 싶은데 아무도 스프레드시트 작성을 안해줘 엉엉</p>\
+<p>갹</p>\
 ";
 };
 
 function loadmemo() {
    $("#bonmoon").empty();
-   bonmoon.innerHTML = "<p>메모입니다.</p>"
+   bonmoon.innerHTML = "<p>메모입니다. 갹.</p>"
 }
